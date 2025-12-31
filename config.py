7b26a11689a38ed644233bd7705008ba913d4103
adi960280-cmd <adityamalhotra960@gmail.com>
2025-12-31 19:16:39 +0530

    Remove sensitive API credentials
    Removed sensitive API credentials from config.py.

--- config.py
+++ config.py
@@ -1,5 +1,1 @@
-import os
 
-API_ID = os.environ.get("API_ID", "29115102") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7998257759:AAFGOPn-12Mkjtz4uTbuEDNMkM9jNRRjK9c") #Replace With your bot token
