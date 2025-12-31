19388b5158338aeea29ea1f93d2224b249ebd01d
adi960280-cmd <adityamalhotra960@gmail.com>
2025-12-31 18:20:25 +0530

    Update API credentials in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-API_ID = os.environ.get("API_ID", "26271673") #Replace With your api id
-API_HASH = os.environ.get("API_HASH", "0e807111856890e4770b3e5a3324ec5f") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7793761896:AAFPGWgzN06FppH66RtQCXSDtPkha7b45vI") #Replace With your bot token
+API_ID = os.environ.get("API_ID", "29115102") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "1a331db2b00e9d2decaa9c7276449eb6") #Replace With your api hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8524464464:AAFnlBCharMOaMYCNMcBKIdajTg_hcJiWec") #Replace With your bot token
